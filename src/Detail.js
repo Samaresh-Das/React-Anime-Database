22a417833cd9eeab38675ade6ab27710d0081f20
Samaresh Das <samareshmail679@gmail.com>
2022-04-07 19:49:47 +0530

    improved the styling

--- src/Detail.js
+++ src/Detail.js
@@ -1,32 +1,31 @@
 import React from "react";
 
 export default function Detail({ film }) {
-  const style = {
-    padding: 10
-  };
+
 
   if (!film) {
     return <div  className = 'details'>Select a film</div>;
   }
   return (
-    <div style={style} className = 'details'>
+    <div className = 'details'>
       
       <div className = 'left'>
         <h2>{film.title}</h2>
         <img src={film.image} alt = 'banners'/>
-        <img src={film.movie_banner} alt = 'banners' className='banner'/>
-      </div>
-      <div className='right'>
-        <p>{film.description}</p>
-        <div>
+        <div className = 'stat'>
           <b>Director</b>: {film.director}
         </div>
-        <div>
+        <div className = 'stat'>
           <b>Producer</b>: {film.producer}
         </div>
-        <div>
+        <div className = 'stat'>
           <b>Released</b>: {film.release_date}
-        </div>
+        </div>        
+      </div>
+      <div className='right'>
+      <img src={film.movie_banner} alt = 'banners' className='banner'/>
+        <p>{film.description}</p>
+        
       </div>
       
     </div>
